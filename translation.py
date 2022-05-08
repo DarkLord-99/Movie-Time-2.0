bb4b4003fa439661a86c93e203bd25ea149ce87c
MoulikMaity <104905898+MoulikMaity@users.noreply.github.com>
2022-05-08 13:24:24 +0530

    translation.py

---
+++ translation.py
@@ -0,0 +1,81 @@
+class MovieTime(object):
+
+    DEFAULT_MSG = """👋Hello {mention}.....!!!\nIt's Power Full [{bot_name}](t.me/{bot_username}) Here 😎\nAdd Me To Your Group And Make Sure I'm an Admin There! \nAnd Enjoy My Pever Show.....!!!🤪"""
+
+    HELP_MSG = """**✅ DO
+👉 Type Only In English
+
+❌ DON'T
+👉 Avoid Symbols (/.,:;'_-!)
+👉 Avoid Requesting Same Movie/Series Repeatedly
+👉 Avoid Requesting Unreleased Movie/Series**"""
+
+    ABOUT_MSG = """
+🤖 My Name [[{bot_name}]](t.me/{bot_username})
+
+🤴 My Dev : [[DarkLord]](https://t.me/hellodarklord)
+
+🧑‍💻 My Creator : [{dev_name}]
+
+🛠️ My Library : [Pyrogram]
+
+🈵 My Language : [Python 3]
+
+🗄️ My Database : [[Mongodb]](https://mongodb.com)
+
+📡 My Server : [[Okteto]](https://okteto.com)"""
+
+    FILE_CAPTIONS = """📂 File Info - <code>{title}</code>
+✯ ━━━━━ ✧ ━━━━━ ✯
+🔘𝐉𝐨𝐢𝐧
+👉<a href=https://t.me/+1CZ0RvqgItEzZTQ1  >𝑀𝑜𝑣𝑖𝑒 𝑇𝑖𝑚𝑒 2.0</a>
+👉<a href=https://t.me/+ZgWO7gGleUgwMTc1 >𝑀𝑜𝑣𝑖𝑒 𝑇𝑖𝑚𝑒 𝑈𝑝𝑑𝑎𝑡𝑒𝑠</a>
+✯ ━━━━━ ✧ ━━━━━ ✯
+➠𝐇𝐞𝐫𝐞 𝐈𝐬 𝐘𝐨𝐮𝐫 #𝐑𝐞𝐪𝐮𝐞𝐬𝐭
+✯ ━━━━━ ✧ ━━━━━ ✯
+👋 ʜᴇʏ!!
+ᴋɪɴᴅʟʏ ᴀᴅᴅ ʏᴏᴜʀ ꜰʀɪᴇɴᴅꜱ, 
+ᴀꜰᴛᴇʀ ɢᴇᴛᴛɪɴɢ ᴍᴏᴠɪᴇꜱ/ꜱᴇʀɪᴇꜱ.
+ɪᴛ ᴡɪʟʟ ʜᴇʟᴘ ᴜꜱ ɢʀᴏᴡɪɴɢ 🙏
+✯ ━━━━━ ✧ ━━━━━ ✯
+⬆️𝐔𝐩𝐥𝐨𝐚𝐝𝐞𝐝 𝐁𝐲
+👉<a href='https://t.me/helloalluarjun'>𝐴𝑙𝑙𝑢 𝐴𝑟𝑗𝑢𝑛</a>
+✯ ━━━━━ ✧ ━━━━━ ✯
+𝐉𝐨𝐢𝐧🎗️ 𝐒𝐡𝐚𝐫𝐞🎗️ 𝐒𝐮𝐩𝐩𝐨𝐫𝐭
+✯ ━━━━━ ✧ ━━━━━ ✯"""
+
+    MoulikMaity = """
+Hello👋 {mention},
+
+I Can Guide You Through All Of <a href='http://t.me/MovieTime_Robot'>Movie Time Bot</a>'s Cool Features & How To Properly Use Them. Use The Buttons Below To Navigate Through All Of The Modules."""
+
+
+ 
+ 
+ 
+
+    ADD_YOUR_GROUP = """**__Join the group I work for and request all your favorite movies in that group! Don't request here.👇__**"""
+    SPELL_CHECK = """
+**Hello 👋{mention}**,
+
+**Couldn't Find {query}?  Please Click Your Request Movie Name**"""
+    GET_MOVIE_1 = """
+**Hello 👋 {mention}**,Good Night
+
+↪️ **Requested** : [{query}]({url})
+👤 **Requested By** : {mention}
+🎬 **Title** : [{title}]({url})
+📆 **Year** : {year}
+🌟 **Rating** : {rating}/10
+🎭 **Genres** : {genres}
+🧑‍🔧 Get Support ✔️ <a href='https://t.me/hellodarklord'>DarkLord</a>"""
+
+    GET_MOVIE_2 = """
+↪️ **Requested** : {query}
+👤 **Requested By** : {mention}
+🎬 Title : [{title}]({url})
+📆 Year : {year}
+🌟 Rating : {rating}/10
+🎭 Genres : {genres}
+
+© **{chat}**"""
